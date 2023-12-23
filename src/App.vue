282c51fa205199fff013c9e824e06ce1d1698bc9
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2023-12-23 12:45:51 +0100

    Moving of panel was added

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,73 @@
 <script setup>
+import { ref } from 'vue';
 
+const panelX = ref(100);
+const panelY = ref(100)
+const panel = ref(null);
+const dragElement = ref(null);
+const isPanelMoving = ref(false);
+
+function startMovingPanel(e)
+{
+  isPanelMoving.value = true;
+}
+function stopMovingPanel(e)
+{
+  isPanelMoving.value = false;
+}
+function movePanel(e)
+{
+  if(isPanelMoving.value)
+  {
+    
+    let diffX = (dragElement.value.getBoundingClientRect().x - panel.value.getBoundingClientRect().x);
+    let diffY = (dragElement.value.getBoundingClientRect().y-panel.value.getBoundingClientRect().y);
+   
+    panelX.value = e.clientX-diffX;
+    panelY.value = e.clientY-diffY - dragElement.value.offsetWidth;
+    
+  }
+}
 </script>
 
 <template>
-  <nav class="panel is-info instruments-panel">
+  <div @mousemove="movePanel" @mouseup="stopMovingPanel">
+    <nav ref="panel" class="panel is-info instruments-panel" :style="{top:`${panelY}px`, left:`${panelX}px`}">
       <div class="panel-heading">
-        <p>
-        Repositories
+          <p>
+          Repositories
+          </p>
+          <span ref="dragElement" class="drag-element" @dragstart="test" @mousemove="movePanel" @mousedown="startMovingPanel" >
+            <font-awesome-icon icon="grip-vertical" />
+          </span>
+        </div>
+        
+        <p class="panel-tabs">
+          <a class="is-active">Elements</a>
+          <a>Algorithms</a>
+          
         </p>
-        <span class="drag-element">
-          <font-awesome-icon icon="grip-vertical" />
-        </span>
-      </div>
-      
-      <p class="panel-tabs">
-        <a class="is-active">Elements</a>
-        <a>Algorithms</a>
         
-      </p>
+        <a class="panel-block">
+          <span class="panel-icon">
+            <font-awesome-icon icon="circle" />
+          </span>
+          Vertice
+        </a>
       
-      <a class="panel-block">
-        <span class="panel-icon">
-          <font-awesome-icon icon="circle" />
-        </span>
-        Vertice
-      </a>
-     
-      
-      <div class="panel-block buttons-block">
-        <button class="button is-link is-outlined is-fullwidth">
-          Save
-        </button>
         
-        <button class="button is-link is-outlined is-fullwidth">
-          Open
-        </button>
-      </div>
-    </nav>
-    <svg></svg>
-  
+        <div class="panel-block buttons-block">
+          <button class="button is-link is-outlined is-fullwidth">
+            Save
+          </button>
+          
+          <button class="button is-link is-outlined is-fullwidth">
+            Open
+          </button>
+        </div>
+      </nav>
+      <svg></svg>
+  </div>
 </template>
 
 <style scoped>

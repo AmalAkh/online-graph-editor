680af10f05fe6a806b70e9a7a3ba1a6e9deb0f8a
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2024-08-26 13:13:08 +0500

    Resizing bug solved

--- src/components/DirectedEdgeSVG.vue
+++ src/components/DirectedEdgeSVG.vue
@@ -48,12 +48,12 @@ const bezierPointY = computed(()=>
 const triangleCoords = computed(()=>
 {
    
-    return `${props.element.x[1]+2}, ${props.element.y[1]}, ${props.element.x[1]-22}, ${props.element.y[1]+6}, ${props.element.x[1]-22}, ${props.element.y[1]-6}`
+    return `${(props.element.x[1]+2)*editorDataStore.zoom}, ${props.element.y[1]*editorDataStore.zoom}, ${(props.element.x[1]-22)*editorDataStore.zoom}, ${(props.element.y[1]+6)*editorDataStore.zoom}, ${(props.element.x[1]-22)*editorDataStore.zoom}, ${(props.element.y[1]-6)*editorDataStore.zoom}`
 })
 const triangleRotationAngle = computed(()=>
 {
     
-    let angle = angleBetween2Vectors(bezierPointX.value,bezierPointY.value, props.element.x[1], props.element.y[1]);
+    let angle = angleBetween2Vectors(bezierPointX.value,bezierPointY.value, props.element.x[1]*editorDataStore.zoom, props.element.y[1]*editorDataStore.zoom);
     if(props.element.y[1] < bezierPointY.value)
     {
         angle*=-1;
@@ -65,7 +65,7 @@ const triangleRotationAngle = computed(()=>
     
     
     <path class="edge" stroke="black" :class="{'selected':editorDataStore.selectedElement?.id == props.element.id}"  @click.stop="select"  :d="`M ${element.x[0]*editorDataStore.zoom} ${element.y[0]*editorDataStore.zoom} Q ${element.bezierPointX*editorDataStore.zoom} ${element.bezierPointY*editorDataStore.zoom} ${element.x[1]*editorDataStore.zoom} ${element.y[1]*editorDataStore.zoom}`" :stroke-width="4*editorDataStore.zoom" fill="none" />
-    <polygon  :class="{'selected':editorDataStore.selectedElement?.id == props.element.id}" :points="triangleCoords" fill="black" :transform="`rotate(${triangleRotationAngle},${props.element.x[1]},${props.element.y[1]})`" />
+    <polygon  :class="{'selected':editorDataStore.selectedElement?.id == props.element.id}" :points="triangleCoords" fill="black" :transform="`rotate(${triangleRotationAngle},${props.element.x[1]*editorDataStore.zoom},${props.element.y[1]*editorDataStore.zoom})`" />
     <circle class="bezier-point" :class="{'active':editorDataStore.selectedElement?.id == props.element.id && editorDataStore.draggingMode == 'edge'}" @click.stop="" @mousedown.stop="startMovingBezierPoint" @mouseup.stop="stopMovingBezierPoint" v-if="editorDataStore.selectedElement?.id == element.id"  :cx="bezierPointX" :cy="bezierPointY" stroke="gray"  r="7"  fill="white" />
 </template>
 <style lang="scss" scoped>

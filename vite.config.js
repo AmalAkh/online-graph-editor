7048e8a833aeae17969bba3fd2c0a83384722fcd
Amal Akhmadinurov <42867705+AmalAkh@users.noreply.github.com>
2024-09-18 13:49:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"/",
+  base:"./",
   server:{port:8000},
   preview:{
     host:true,
